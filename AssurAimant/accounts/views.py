a520f4d32283c0b5c590c52957f63dee5c7591b4
HerculePoivrot <delvoyeadf@gmail.com>
2025-01-27 14:46:24 +0100

    Ajout nom, prenom, email sur formulaire inscription

--- AssurAimant/accounts/views.py
+++ AssurAimant/accounts/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
 from django.views.generic import CreateView
-
+from django.urls import reverse_lazy
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
