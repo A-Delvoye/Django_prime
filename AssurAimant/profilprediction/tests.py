6248b7efa843aa715e8002727c2135aea19a53ce
gvannesson <g.vannesson@gmail.com>
2025-05-15 15:22:08 +0200

    pytest 1

--- AssurAimant/profilprediction/tests.py
+++ AssurAimant/profilprediction/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from .models import ProfilePrediction, Prediction
+# from django.contrib.auth.models import User
 
-# Create your tests here.
+# class PredictionModelTest(TestCase):
+#     @classmethod
+#     def setUpTestData(cls):
+#         cls.user = User.objects.create_user(username='gauthier3', password='azerty1234!')
+
+#     def setUp(self):
+#         self.profilepredict = ProfilePrediction.objects.create(
+#             user = self.user,
+#             age=25,
+#             sex='Male',
+#             bmi=5,
+#             children=2,
+#             smoker=1,
+#             region='Northeast'
+#         )
+
+#     def test_profileprediction_age(self):
+#         self.assertIsInstance(self.profilepredict, ProfilePrediction)
+#         self.assertEqual(self.profilepredict.age, 25)
+#         self.assertGreaterEqual(self.profilepredict.age, 18)
+
+#     def test_profileprediction_bmi(self):
+#         self.assertIsInstance(self.profilepredict, ProfilePrediction)
+#         self.assertEqual(self.profilepredict.bmi, 5)
+#         self.assertGreaterEqual(self.profilepredict.bmi, 4)
